3ed6c07246d89b768e91b4985659e0e3751263e3
Arley Boada <richardrabt@gmail.com>
2026-09-08 16:44:36 -0500

    feat(asistencia): integrar QRCheckAPI con el contrato ServicioAsistencia

---
+++ src/main/java/com/universidad/confudes/asistencia/AdaptadorQRCheck.java
@@ -0,0 +1,29 @@
+package com.universidad.confudes.asistencia;
+
+import com.universidad.confudes.externo.qrcheck.QRCheckClient;
+import com.universidad.confudes.externo.qrcheck.QRCheckRequest;
+import com.universidad.confudes.externo.qrcheck.QRCheckResponse;
+import org.springframework.stereotype.Service;
+
+// Adapter: traduce el contrato de QRCheckAPI (externo) al contrato
+// interno ServicioAsistencia, sin que ControladorCheckIn sepa que existe.
+@Service
+public class AdaptadorQRCheck implements ServicioAsistencia {
+
+    private final QRCheckClient qrCheckClient;
+
+    public AdaptadorQRCheck() {
+        this.qrCheckClient = new QRCheckClient();
+    }
+
+    @Override
+    public ResultadoCheckIn registrarAsistencia(String eventoId, String participanteId, String credencialQR) {
+        long idEvento = Long.parseLong(eventoId);
+
+        QRCheckRequest request = new QRCheckRequest(credencialQR, idEvento);
+        QRCheckResponse response = qrCheckClient.validar(request);
+
+        boolean exitoso = response.getCodigoRespuesta() == 200;
+        return new ResultadoCheckIn(exitoso, response.getDetalle());
+    }
+}
